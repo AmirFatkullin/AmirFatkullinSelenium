dc364ee3330cfe709892b4f52401c6c510e6cb1e
AmirFatkullin <Amir.Fatkullin@e-lab.icl.kazan.ru>
2017-03-16 17:00:57 +0300

    Добавил файл в репозиторий
    Добавил файл в репозиторий

---
+++ java-example/src/test/java/ru/stqa/training/selenium/Moyvtoroytest.java
@@ -0,0 +1,40 @@
+package ru.stqa.training.selenium;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class Moyvtoroytest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait=new WebDriverWait(driver,10);
+    }
+
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://192.168.209.249:8081/suib/index.do");
+        driver.findElement(By.id("LoginUsername")).sendKeys("Andrey");
+        driver.findElement(By.id("loginBtn")).click();
+        //driver.findElement(By.name("q")).sendKeys("webdriver");
+        //driver.findElement(By.name("btnG")).click();
+        //wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver=null;
+    }
+}
