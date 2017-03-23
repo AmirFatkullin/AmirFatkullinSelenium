236d7cda39d2427fc827a176a50050725870fead
AmirFatkullin <Amir.Fatkullin@e-lab.icl.kazan.ru>
2017-03-23 14:18:01 +0300

    nightly

---
+++ java-example/src/test/java/ru/stqa/training/selenium/my5testnightly.java
@@ -0,0 +1,63 @@
+package ru.stqa.training.selenium;
+
+
+        import org.junit.After;
+        import org.junit.Before;
+        import org.junit.Test;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.HasCapabilities;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.firefox.FirefoxBinary;
+        import org.openqa.selenium.firefox.FirefoxOptions;
+        import org.openqa.selenium.firefox.FirefoxProfile;
+        import org.openqa.selenium.remote.DesiredCapabilities;
+        import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+        import org.openqa.selenium.ie.InternetExplorerDriver;
+        import org.openqa.selenium.firefox.FirefoxDriver;
+
+        import java.io.File;
+
+        import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class my5testnightly {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        //driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        driver = new FirefoxDriver(
+                new FirefoxOptions()
+                        .setLegacy(true)
+                        .setBinary(new FirefoxBinary(new File("C:\\Program Files (x86)\\Nightly\\firefox.exe"))));
+        //caps.setCapability(FirefoxDriver.MARIONETTE, false);
+        // driver = new FirefoxDriver(caps);
+        //driver = new FirefoxDriver(
+        //new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")),
+        //new FirefoxProfile(), caps);
+        //driver = new FirefoxDriver(
+        //new FirefoxBinary(new File"C:\\Program Files\\Mozilla Firefox\\firefox.exe")),
+        //new FirefoxProfile(), caps);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
+        wait=new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void my5testnightly() {
+        driver.get("http://litecart.stqa.ru/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("0b7dba1c77df25bf0");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver=null;
+    }
+}
