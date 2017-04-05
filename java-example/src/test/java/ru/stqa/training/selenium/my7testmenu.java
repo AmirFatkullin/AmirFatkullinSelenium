485fe5c022b554964c1c2a6ce8c562975986c693
AmirFatkullin <Amir.Fatkullin@e-lab.icl.kazan.ru>
2017-04-05 11:00:09 +0300

    7 готово

--- java-example/src/test/java/ru/stqa/training/selenium/my7testmenu.java
+++ java-example/src/test/java/ru/stqa/training/selenium/my7testmenu.java
@@ -20,39 +20,61 @@ public class my7testmenu {
     private WebDriver driver;
     private WebDriverWait wait;
 
-
     @Before
     public void start() {
         driver = new ChromeDriver();
         wait=new WebDriverWait(driver,10);
 
-
     }
 
-
     @Test
-    public void my7testmenu() {
-        int i=0;
+    public void zadanie7() {
+        int i,e,n,k;
         driver.get("http://litecart.stqa.ru/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("0b7dba1c77df25bf0");
         driver.findElement(By.name("login")).click();
         wait.until(titleIs("My Store"));
-        List <WebElement> elements= driver.findElements(By.id("app-"));
 
-            elements.get(0).click();
-        List <WebElement> element= driver.findElements(By.id("app-"));
-        for(i=0;i<17;i++) {
-            elements.get(i).click();
+        List <WebElement> elements= driver.findElements(By.cssSelector("li#app-"));
+        List<WebElement> podelements;
+        WebElement element,podelement;
+        n=elements.size();
+
+        for(i=0;i<n;i++)
+        {
+            elements=driver.findElements(By.cssSelector("li#app-"));
+            element=elements.get(i);
+            wait=new WebDriverWait(driver,10);
+            element.click();
+
+            elements=driver.findElements(By.cssSelector("li#app-"));
+            element=elements.get(i);
+            podelements=element.findElements(By.cssSelector("[id^=doc-]"));
+            k=podelements.size();
+
+            if (k>0)
+            {
+                for(e=0;e<k;e++)
+                {
+                    elements=driver.findElements(By.cssSelector("li#app-"));
+                    element=elements.get(i);
+                    podelements=element.findElements(By.cssSelector("[id^=doc-]"));
+                    podelement=podelements.get(e);
+                    podelement.click();
+                    driver.findElement(By.cssSelector("h1"));
+                }
+            }
+            else
+            {
+                driver.findElement(By.cssSelector("h1"));
+            }
         }
-        }
-
-
-
+    }
 
     @After
     public void stop() {
-        //driver.quit();
-        //driver=null;
+        driver.quit();
+        driver=null;
     }
 }
